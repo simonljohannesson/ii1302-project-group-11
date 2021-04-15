7ef578bafd3f79225745d707a5cc23be29dbfa48
vpetters <vpetters@kth.se>
2021-04-15 11:06:17 +0200

    toolbar items for toolbar

---
+++ frontend/webpage/src/components/ToolbarItem.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <router-link :to="{ name: link}">
+            <i :class="this.iconClass"></i> {{link}}
+        </router-link>
+    </div>
+</template>
+<script>
+export default {
+    name: 'ToolbarItem',
+    props: {
+        link: String,       //link to router component passed as prop
+        iconClass: String,  //icon class name on font awesome, pass full class name
+    }
+}
+</script>
+<style >
+    i {
+        padding-right: 10px;
+    }
+</style>
