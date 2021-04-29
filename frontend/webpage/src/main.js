e4983cd9caf7a81da0592a45aff64704e4139e19
virsp <victor.pettersson@gmail.com>
2021-04-29 08:58:58 +0200

    only start Mirage when in dev mode

--- frontend/webpage/src/main.js
+++ frontend/webpage/src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import { analytics } from './api/firebase.js'
+import { startMirage } from './mirage'
+
+if(process.env.NODE_ENV === 'development'){
+    console.log("Starting Mirage API mocking");
+    startMirage();
+}
 
 analytics.logEvent('login');
 createApp(App).use(store).use(router).mount('#app')
