03078d52be5e4766b045484939f31d4aff7f634c
virsp <victor.pettersson@gmail.com>
2021-04-29 09:50:39 +0200

    api mocking tests

---
+++ frontend/webpage/tests/unit/mirage.spec.js
@@ -0,0 +1,60 @@
+import { startMirage } from '../../src/mirage'
+
+/**
+ * Setup a mirage server and closing it before every run to 
+ * eliminate possible interferances between tests
+ */
+let mirageServer;
+beforeEach(() => {
+    mirageServer = startMirage();
+    // Dissable logging to not bloat jest test output
+    mirageServer.logging = false;
+})
+afterEach(() => {
+    mirageServer.shutdown()
+})
+
+test('Faulty request to /room_count', async() => {
+    const apiEndpoint = 'https://service.eu.apiconnect.ibmcloud.com/gws/apigateway/api/86245fabd6001646aa3167f3aaec32059fe9170e90e8fa6f6334f2020bd5b33b/bodycount/room_count';
+    let resCode;
+    await fetch(apiEndpoint, {
+        method: 'get',
+
+    }).then((res) => {
+        resCode = res.status;
+    })
+    expect(resCode).toBe(400);
+})
+test('Correct request', async() => {
+    const apiEndpoint = 'https://service.eu.apiconnect.ibmcloud.com/gws/apigateway/api/86245fabd6001646aa3167f3aaec32059fe9170e90e8fa6f6334f2020bd5b33b/bodycount/room_count';
+    let resCode;
+    await fetch(apiEndpoint + '?room_name=bastu&user_name=albert', {
+        method: 'get',
+
+    }).then((res) => {
+        resCode = res.status;
+    })
+    expect(resCode).toBe(200);
+})
+test('Missing room_name param', async() => {
+    const apiEndpoint = 'https://service.eu.apiconnect.ibmcloud.com/gws/apigateway/api/86245fabd6001646aa3167f3aaec32059fe9170e90e8fa6f6334f2020bd5b33b/bodycount/room_count';
+    let resCode;
+    await fetch(apiEndpoint + '?user_name=albert', {
+        method: 'get',
+
+    }).then((res) => {
+        resCode = res.status;
+    })
+    expect(resCode).toBe(400);
+})
+test('Missing user_name param', async() => {
+    const apiEndpoint = 'https://service.eu.apiconnect.ibmcloud.com/gws/apigateway/api/86245fabd6001646aa3167f3aaec32059fe9170e90e8fa6f6334f2020bd5b33b/bodycount/room_count';
+    let resCode;
+    await fetch(apiEndpoint + '?room_name=bastu', {
+        method: 'get',
+
+    }).then((res) => {
+        resCode = res.status;
+    })
+    expect(resCode).toBe(400);
+})
