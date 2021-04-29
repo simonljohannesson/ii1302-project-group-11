96efc5c30263bf98c1e766b69aa98b9e1ecbe667
Simon Johannesson <simonljohannesson@gmail.com>
2021-04-29 07:39:27 +0200

    Added prototype SearchRoom component and relevant functionality in store.

--- frontend/webpage/src/store/index.js
+++ frontend/webpage/src/store/index.js
@@ -1,11 +1,56 @@
 import { createStore } from 'vuex'
+import { ibmAPI } from "../api/ibm.js"
 
 export default createStore({
   state: {
+    userName: "someadmin",
+    /* information about rooms */
+    searchResult : {error: "no search results"},//null,
+    storedSearchResults : [
+    {
+      last_updated: '2021-04-21T07:01:48.387Z',
+      room_count: 14,
+      room_name: 'somename1'
+    },
+    {
+      last_updated: '2021-04-21T07:01:48.387Z',
+      room_count: 14,
+      room_name: 'somename2'
+    },
+    {
+      last_updated: '2021-04-21T07:01:48.387Z',
+      room_count: 14,
+      room_name: 'somename3'
+    }
+    ]
   },
   mutations: {
+    setSearchResult(state, roomInfo){
+      state.searchResult = roomInfo;
+    }
   },
   actions: {
+    GET_ROOM_COUNT(state, roomName, userName){
+      ibmAPI.getRoomCount(roomName, userName)
+      .then((data=>{
+        state.commit("setSearchResult", res)
+      }))
+    },
+    SET_LAST_ROOM_SEARCH(state, roomName){
+      console.log('Setting the last room search manually.')
+      state.commit("setSearchResult", {last_updated: "12:31", room_count: 66, room_name: roomName})
+    }
+  },
+  getters:{
+    getSearchResult(state){
+      return state.searchResult;
+    },
+    getStoredSearchResults(state){
+      return state.storedSearchResults;
+    },
+    getUserName(state){
+      return state.userName;
+    }
   },
   modules: {
   }
---
+++ frontend/webpage/src/views/SearchRoom.vue
@@ -0,0 +1,78 @@
+<template >
+    <div>
+        <div >
+            <h1>Rooms</h1>
+        <div>
+            <div v-if="storedSearchResultsExists">Stored search results
+                <div v-for="item in getStoredSearchResults" :key="item.room_name">
+                    <span> {{item}} </span>
+                </div>
+            </div>
+            <span v-else>No search results stored.</span>
+            <span></span>
+        </div>
+        </div>
+            <input v-model="searchInput" placeholder="Enter a room name." @keyup.enter="searchRoom">
+            <a class="search noselect" v-on:click="searchRoom" >Search</a>
+        <div>
+        <div>{{searchInput}}</div>
+        <div v-if="lastSearchExists()">{{getLastSearch}}</div>
+        <div v-else>Search not made yet.</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data : ()=> {
+        return  {
+            searchInput: ""
+        }
+    },
+    methods : {
+        test : () => console.log(new Date()),
+        lastSearchExists(){
+            if(this.$store.getters.getSearchResult === null){
+                return false;
+            }else{
+                return true;
+            }
+        },
+        storedSearchResultsExists(){
+            if(this.$store.getters.getStoredSearchResults.length > 0){
+                return true;
+            }else{
+                return false;
+            }
+        },
+        searchRoom(){
+            console.log("calling searchRoom with room name: " + this.searchInput + " username: " + this.$store.getters.getUserName)
+            this.$store.dispatch("SET_LAST_ROOM_SEARCH", this.searchInput)
+        }   
+    },
+    computed: {
+        getLastSearch(){
+            return this.$store.getters.getSearchResult;
+        },
+        getStoredSearchResults(){
+            return this.$store.getters.getStoredSearchResults;
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+.search{
+    text-decoration: none;
+    background-color: var(--bc-yellow);
+    /* background-color: #b4dfe5; */
+    color: var(--bc-blue);
+    padding: 0.2em 1em;
+}
+.search:hover{
+    cursor: pointer;
+    color: var(--bc-blue);
+    background-color: var(--bc-orange);
+}
+</style>
