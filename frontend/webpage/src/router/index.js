1fd20e308c03c4e713e24438eb2a719d474f6824
Simon Johannesson <simonljohannesson@gmail.com>
2021-04-30 10:15:43 +0200

    Fixed incorrect setup.

--- frontend/webpage/src/router/index.js
+++ frontend/webpage/src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 import Welcome from '../views/Welcome.vue'
-import RoomCount from '../views/SearchRoom.vue'
+import SearchRoom from '../views/SearchRoom.vue'
 
 const routes = [
   {
@@ -23,7 +23,7 @@ const routes = [
   {
     path: '/searchroom',
     name: 'SearchRoom',
-    component: RoomCount
+    component: SearchRoom
   }
 ]
 
