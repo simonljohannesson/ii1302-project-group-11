4008629f3c9445a000de49ff1a0f0057e12a6487
Simon Johannesson <simonljohannesson@gmail.com>
2021-05-03 17:40:49 +0200

    Refactor for easier testing.

--- frontend/webpage/src/store/index.js
+++ frontend/webpage/src/store/index.js
@@ -1,64 +1,75 @@
 import { createStore } from 'vuex'
 import { ibmAPI } from "../api/ibm.js"
 
+const state = {
+  /* 
+    user identifyer, currently used when making api calls for the room count 
+    should not be hard coded
+  */
+  userName: "someadmin",
+  /* information about rooms */
+  searchResult: null,
+  /* Array with search results of the object with the format:
+    {
+        last_updated: '2021-04-21T07:01:48.387Z',
+        room_count: 14,
+        room_name: 'somename1'
+    }
+  */
+  storedSearchResults: []
+};
+
+export const mutations = {
+  setSearchResult(state, roomInfo) {
+    state.searchResult = roomInfo;
+  },
+};
+
+export const actions = {
+  /**
+   * Search for the room count information of a room.
+   * 
+   * Return obeject format: 
+   * {
+   *    last_updated: '2021-04-21T07:01:48.387Z',
+   *    room_count: 14,
+   *    room_name: 'somename1'
+   * }
+   * or 
+   * {error: "..."}
+   * 
+   * @param {vuex context} context 
+   * @param {String} roomName 
+   * @returns {Object}
+   */
+  SEARCH_ROOM_COUNT(context, roomName) {
+    ibmAPI.getRoomCount(roomName, context.state.userName)
+      .then((data => {
+        context.commit("setSearchResult", data)
+      }))
+  },
+  TEST(context, exampleSearch){
+    context.commit("setSearchResult", exampleSearch)
+  }
+};
+
+export const getters = {
+  getSearchResult(state) {
+    return state.searchResult;
+  },
+  getStoredSearchResults(state) {
+    return state.storedSearchResults;
+  },
+  getUserName(state) {
+    return state.userName;
+  }
+};
+
 export default createStore({
-  state: {
-    /* 
-      user identifyer, currently used when making api calls for the room count 
-      should not be hard coded
-    */
-    userName: "someadmin",
-    /* information about rooms */
-    searchResult: null,
-    /* Array with search results of the object with the format:
-      {
-          last_updated: '2021-04-21T07:01:48.387Z',
-          room_count: 14,
-          room_name: 'somename1'
-      }
-    */
-    storedSearchResults: []
-  },
-  mutations: {
-    setSearchResult(state, roomInfo) {
-      state.searchResult = roomInfo;
-    },
-  },
-  actions: {
-    /**
-     * Search for the room count information of a room.
-     * 
-     * Return obeject format: 
-     * {
-     *    last_updated: '2021-04-21T07:01:48.387Z',
-     *    room_count: 14,
-     *    room_name: 'somename1'
-     * }
-     * or 
-     * {error: "..."}
-     * 
-     * @param {vuex context} context 
-     * @param {String} roomName 
-     * @returns {Object}
-     */
-    SEARCH_ROOM_COUNT(context, roomName) {
-      ibmAPI.getRoomCount(roomName, context.state.userName)
-        .then((data => {
-          context.commit("setSearchResult", data)
-        }))
-    }
-  },
-  getters: {
-    getSearchResult(state) {
-      return state.searchResult;
-    },
-    getStoredSearchResults(state) {
-      return state.storedSearchResults;
-    },
-    getUserName(state) {
-      return state.userName;
-    }
-  },
+  state,
+  mutations,
+  actions,
+  getters,
   modules: {
   }
 })
