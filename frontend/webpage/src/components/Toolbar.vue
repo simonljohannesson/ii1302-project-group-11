0935e9af8c0031ba6153427a3404807f638f45a3
Simon Johannesson <simonljohannesson@gmail.com>
2021-05-04 14:41:16 +0200

    Fix toolbar name of search page.

--- frontend/webpage/src/components/Toolbar.vue
+++ frontend/webpage/src/components/Toolbar.vue
@@ -5,7 +5,8 @@
         </router-link>
         <ToolbarItem link="Home" iconClass="fa fa-home" />
         <ToolbarItem link="About" iconClass="fa fa-info" />
-        <ToolbarItem link="SearchRoom"/>
+        <ToolbarItem link="Search Room" iconClass="fa fa-search"/>
+
     </div>
 </template>
 <script>
--- frontend/webpage/src/router/index.js
+++ frontend/webpage/src/router/index.js
@@ -22,7 +22,7 @@ const routes = [
   },
   {
     path: '/searchroom',
-    name: 'SearchRoom',
+    name: 'Search Room',
     component: SearchRoom
   }
 ]
