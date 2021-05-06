32af92005dc393d1f4c9aa833a5bf5fd7e367e4e
virsp <victor.pettersson@gmail.com>
2021-05-06 19:41:20 +0200

    added login guarding

--- frontend/webpage/src/router/index.js
+++ frontend/webpage/src/router/index.js
@@ -4,33 +4,35 @@ import About from '../views/About.vue'
 import Welcome from '../views/Welcome.vue'
 import Login from '../views/Login.vue'
 import SearchRoom from '../views/SearchRoom.vue'
+import store from '@/store/index';
 
 const routes = [
-  {
-    path: '/home',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/',
-    name: 'Welcome',
-    component: Welcome
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: About,
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: Login,
-  },
-  {
-      path: '/searchroom',
-      name: 'Search Room',
-      component: SearchRoom
-  }
+    {
+        path: '/home',
+        name: 'Home',
+        component: Home,
+    },
+    {
+        path: '/welcome',
+        name: 'Welcome',
+        component: Welcome,
+      
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: About,
+    },
+    {
+        path: '/',
+        name: 'Login',
+        component: Login,
+    },
+    {
+        path: '/searchroom',
+        name: 'Search Room',
+        component: SearchRoom,
+    }
 ]
 
 const router = createRouter({
@@ -39,4 +41,9 @@ const router = createRouter({
     routes
 })
 
+router.beforeEach((to, from, next) => {
+    const isAuthenticated = store.getters.getLoginToken;
+    if (to.name !== 'Login' && !isAuthenticated) next({ name: 'Login' });
+    else next();
+})
 export default router
